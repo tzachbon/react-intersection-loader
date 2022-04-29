6ea02e27bbee8b294ddc75a632ae47d80d02421b
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2022-04-29 13:15:46 +0300

    fix: use react lazy fallback for force

--- packages/react-intersection-loader/src/index.tsx
+++ packages/react-intersection-loader/src/index.tsx
@@ -1,21 +1,80 @@
-import { ComponentType, useLayoutEffect, useRef, useState } from 'react';
+import { ComponentType, useCallback, useLayoutEffect, useRef, useState, lazy, Suspense, ReactNode } from 'react';
 
 export type ComponentModule<T extends {}> = { default: ComponentType<T> } | ComponentType<T>;
 
 export interface InteractionLoaderOptions<T extends {}> {
+  /**
+   * Function that must call a dynamic import().\
+   * This must return a Promise which resolves to a module with a React component (it could be default export).
+   *
+   * @example
+   *
+   * // It has default import
+   * () => import('./MyComponent')
+   *
+   * @example
+   *
+   * // Directly return a module with a component
+   * () => import('./MyComponent').MyComponent
+   */
   load: () => Promise<ComponentModule<T>>;
+  /**
+   * Intersection observer options.\
+   * https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserver/IntersectionObserver#parameters
+   */
   intersectionObserverOptions?: IntersectionObserverInit;
+  /**
+   * The component to render when the element is not visible.
+   * This uses `React.lazy`
+   */
+  force?: boolean;
+  /**
+   * Only when using `force` this is the fallback react node that will be used in the Suspense.
+   */
+  fallback?: ReactNode;
 }
 
 export function intersectionLoader<T extends {}>({
   load,
   intersectionObserverOptions,
+  force,
+  fallback,
 }: InteractionLoaderOptions<T>): ComponentType<T> {
   return function (props: T) {
     const root = useRef<HTMLDivElement>(null);
-    const [Component, setState] = useState<ComponentType<T>>();
+    const [Component, setComponent] = useState<ComponentType<T>>();
+
+    const loadComponent = useCallback(async () => {
+      const component = interopDefault(await load());
+
+      if (!component) {
+        setComponent(() => {
+          throw new Error('Component is not defined');
+        });
+        return;
+      }
+
+      setComponent(() => component);
+    }, []);
 
     useLayoutEffect(() => {
+      if (force) {
+        setComponent(() => {
+          const LazyComponent = lazy(() =>
+            load().then((mod) => ({
+              default: interopDefault(mod) as ComponentType<any>,
+            }))
+          );
+
+          return (props: T) => (
+            <Suspense fallback={fallback}>
+              <LazyComponent {...props} />
+            </Suspense>
+          );
+        });
+        return;
+      }
+
       const observer = new IntersectionObserver(
         async (entries) => {
           for (const entry of entries) {
@@ -24,17 +83,7 @@ export function intersectionLoader<T extends {}>({
             }
 
             observer.disconnect();
-
-            const Component = interopDefault(await load());
-
-            if (!Component) {
-              setState(() => {
-                throw new Error('Component is not defined');
-              });
-              return;
-            }
-
-            setState(Component);
+            await loadComponent();
           }
         },
         { threshold: 0.1, ...intersectionObserverOptions }
@@ -43,11 +92,11 @@ export function intersectionLoader<T extends {}>({
       observer.observe(root.current!);
 
       return () => {
-        observer.disconnect();
+        observer?.disconnect();
       };
     }, []);
 
-    return Component ? (
+    return Component !== undefined ? (
       <Component {...props} />
     ) : (
       <div dangerouslySetInnerHTML={{ __html: '' }} suppressHydrationWarning ref={root}></div>
